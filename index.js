8a45029a6958e42dd98c38abbe1ab712c5872823
AdeolaBamisile <theacutesniper@gmail.com>
2026-05-16 00:12:51 +0100

    uibuild

--- index.js
+++ index.js
@@ -4,24 +4,6 @@ const morgan = require('morgan')
 //const cors = require('cors')
 const app = express()
 
-let notes = [
-    {
-        id: "1",
-        content: "HTML is easy",
-        important: true
-    },
-    {
-        id: "2",   
-        content: "Browser can execute only JavaScript",
-        important: false
-    },
-    {
-        id: "3",
-        content: "POST and GET are the most important methods of HTTP protocol",
-        important: true
-    }
-]
-
 //app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
@@ -42,36 +24,40 @@ const requestLogger = (request, response, next) => {
 app.get('/api/notes', (request, response) => {
     Note.find().then(notes => {
         response.json(notes)
+    }).catch(error => {
+        response.status(400).json({error: "Couldn't fetch notes"})
     })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/notes/:id', (request, response, next) => {
     Note.findByIdAndDelete(request.params.id).then(note => {
         response.status(204).end()
-    })
+    }).catch(error => next(error))
 })
 
-app.patch('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    const body = request.body
-    const note = notes.find(note => note.id === id)
-    if (!note) {
-        return response.status(404).json({error: 'Note not found'})
-    }
-    const updatedNote = {...note, ...body}
-    notes = notes.map(note => note.id == id ? updatedNote : note)
-    response.json(updatedNote)
+app.put('/api/notes/:id', (request, response, next) => {
+    const {content, important} = request.body
+    Note.findById(request.params.id).then(note => {
+        if (!note) {
+            return response.status(404).end()
+        }
+        note.content = content
+        note.important = important
+        return note.save().then(updatedNote => {
+            response.json(updatedNote)
+        })
+    }).catch(error => next(error))
 })
 
 
 const generateId = () => {
     const maxId = notes.length > 0 
-        ? Math.max(...notes.map(note => Number(note.id)))
-        : 0
+    ? Math.max(...notes.map(note => Number(note.id)))
+    : 0
     return(String(maxId + 1))
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
     const body = request.body
     if (!body.content) {
         return response.status(400).json({
@@ -82,21 +68,42 @@ app.post('/api/notes', (request, response) => {
         content: body.content,
         important: body.important || false,
     })
-
+    
     note.save().then(savednote => {
         response.json(savednote)
-    })
+    }).catch(error => next(error))
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-        response.json(note)
+        if (note) {
+            response.json(note)
+        } else {
+            response.status(404).end()
+        }
+    }).catch(error => {
+        next(error)
+        // console.log(error.message)
+        // response.status(400).send({error: 'Malformatted Id'})
     })
 })
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({error: 'unknown endpoint'})
 }
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name == 'CastError') {
+        response.status(404).json({error: 'malformatted id'})
+    }
+    next(error)
+}
+
+app.use(errorHandler)
 
 let PORT = process.env.PORT
 app.listen(PORT, () => {
