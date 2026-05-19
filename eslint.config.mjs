bb50ca51d2a6bbf40369bc1e45917f2e1ee42cbd
AdeolaBamisile <theacutesniper@gmail.com>
2026-05-19 23:02:05 +0100

    uibuild

---
+++ eslint.config.mjs
@@ -0,0 +1,20 @@
+import globals from "globals";
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import stylisticJS from "@stylistic/eslint-plugin"
+import { plugin } from "mongoose";
+
+export default defineConfig([
+  js.configs.recommended,
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  { plugins: {'@stylistic/js': stylisticJS} },
+  { rules: {
+    '@stylistic/js/indent': ['error', 2],
+    '@stylistic/js/linebreak-style': ['error', 'unix'],
+    '@stylistic/js/quotes': ['error', 'single'],
+    '@stylistic/js/semi': ['error', 'never'],
+    eqeqeq: 'error'
+  }},
+  { ignores: ['dist/**'] }
+]);
